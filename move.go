7d6c5ace20a00b4807ca917ae97ca313b2b342fa
xlvector <xlvector@gmail.com>
2016-03-12 23:52:24 +0800

    lr model: mcts, copy

--- move.go
+++ move.go
@@ -557,10 +557,9 @@ func (b *Board) GenQuickMove(lastMove Point, stone Color) Point {
 	pr := rand.Float64() * psum
 	for k, v := range movs {
 		pr -= v
-		if psum <= 0.0 {
+		if pr <= 0.0 {
 			p := b.w[k]
 			if err := b.Put(p.x, p.y, stone); err == nil {
-				log.Println(b.w[k].String())
 				return p
 			}
 		}
