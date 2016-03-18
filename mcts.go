578699122c727c67f040a6a6aeb12ca719e45654
xlvector <xlvector@gmail.com>
2016-03-18 21:08:33 +0800

    mcts

--- mcts.go
+++ mcts.go
@@ -186,11 +186,9 @@ func MCTSSelection(gt *GameTree) *GameTreeNode {
 	depth := 0
 	for {
 		if ret.Children == nil || len(ret.Children) == 0 {
-			log.Println(depth, PointString(ret.x, ret.y, ret.stone))
 			return ret
 		}
 		if len(ret.CandMoves) > 0 {
-			log.Println(depth, PointString(ret.x, ret.y, ret.stone))
 			return ret
 		}
 		depth += 1
