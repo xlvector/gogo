28dc40728388199270d2136afd3ccbff08a24059
xlvector <xlvector@gmail.com>
2016-03-22 09:19:13 +0800

    pair wise

--- gen_pattern.go
+++ gen_pattern.go
@@ -117,7 +117,7 @@ func (b *Board) GenPattern(sgf string, rotate int) []*PairwiseSample {
 		return []*PairwiseSample{}
 	}
 	path := gt.Path2Root()
-	ret := make([]*PairwiseSample, 12)
+	ret := make([]*PairwiseSample, 0, 6)
 	lastPat := []int64{}
 	for i := len(path) - 2; i >= 0; i-- {
 		cur := path[i]
@@ -129,7 +129,7 @@ func (b *Board) GenPattern(sgf string, rotate int) []*PairwiseSample {
 		curPat := b.FinalPatternHash(curK, cur.stone)
 		curPf := &PatternSample{b.PatternFeature(curK, cur.stone, lastPat, curPat), 1}
 
-		vps := b.RandomSelectValidPoint(5, cur.stone)
+		vps := b.RandomSelectValidPoint(2, cur.stone)
 		for p, _ := range vps {
 			if p == curK {
 				continue
