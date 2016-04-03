aa6bea895c2e78244519986d040a817c804af615
xlvector <xlvector@gmail.com>
2016-04-03 21:53:32 +0800

    roll out

--- mcts.go
+++ mcts.go
@@ -326,17 +326,20 @@ func (b *Board) GenSelfBattleMove(c Color, lgr *LastGoodReply) int {
 
 	for i := 0; i < NPOINT*2; i++ {
 		k := rand.Intn(NPOINT)
-		/*
-			if b.Model != nil {
-				pat := b.FinalPatternHash(k, c)
-				smp := b.PatternFeature(k, c, b.LastPattern, pat)
-				sample := core.NewSample()
-				for _, v := range smp {
-					sample.AddFeature(core.Feature{v, 1.0})
-				}
-				pr = b.Model.Predict(sample)
+
+		if i < 10 && b.Model != nil {
+			pat := b.FinalPatternHash(k, c)
+			smp := b.PatternFeature(k, c, b.LastPattern, pat)
+			sample := core.NewSample()
+			for _, v := range smp {
+				sample.AddFeature(core.Feature{v, 1.0})
 			}
-		*/
+			pr := b.Model.Predict(sample)
+			if pr < 0.1 {
+				continue
+			}
+		}
+
 		if ok, _ := b.CanPut(k, c); ok {
 			b.Put(k, c)
 			return k
